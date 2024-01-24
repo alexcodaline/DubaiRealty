b8b530f376a27fbbb87d14906aadc20c015efa18
alexcodaline <aleksander.ponom@gmail.com>
2024-01-24 13:51:12 +0200

    fix preload

--- src/components/Preload/Preload.jsx
+++ src/components/Preload/Preload.jsx
@@ -7,11 +7,17 @@ export default function Preload() {
             <div className='preload-logo'>
                 <div className='preload-img'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="126" height="150" viewBox="0 0 126 387" fill="none">
-                        <path d="M126 2L2 2V193.5V385H126" stroke="#FCD54C" stroke-width="15" />
+                        <path d="M126 2L2 2V193.5V385H126" stroke="#FCD54C" strokeWidth="15" />
                     </svg>
                 </div>
-                <div className='preload-name'>Dubai Realty</div>
-                <div className='preload-desc'>Real Estate</div>
+                <div className='preload-name-container'>
+                    <div className='preload-name'>Dubai</div>
+                    <div className='preload-name-second'>Realty</div>
+                </div>
+                <div className="preload-desc-container">
+                    <div className='preload-desc'>Real</div>
+                    <div className='preload-desc-second'>Estate</div>
+                </div>
             </div>
         </section>
     )
